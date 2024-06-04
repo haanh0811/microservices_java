118796c9bdf974eceeeaf6adf109795949cb2570
Julien Haderer <81695812+JulienHaderer@users.noreply.github.com>
2024-06-04 20:14:51 +0200

    Add log to gateway

--- Gateway/src/main/java/com/example/demo/GatewayApplication.java
+++ Gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
+	private static final Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
